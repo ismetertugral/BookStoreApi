eb5c54784b64b25c4345fb224f3e4e6005f150f9
ismet ERTUGRAL <ismet.ertugral@solmaz.com.tr>
2022-11-24 18:11:03 +0300

    acıklama

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ builder.Services.Configure<BookStoreDatabaseSettings>(
     builder.Configuration.GetSection("BookStoreDatabase"));
 
 builder.Services.AddSingleton<BooksService>();
-
+ 
 builder.Services.AddControllers()
 .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
